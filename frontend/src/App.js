d74a65a0093781ab5fb80f2e6ee997ab09b72394
Kiril <mikasaxd@gmail.com>
2024-03-27 22:06:57 +0200

    create components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,7 @@
 import './App.css';
+import BookForm from './components/BookForm/BookForm';
+import BookList from './components/BookList/BookList';
+import Filter from './components/Filter/Filter';
 
 function App() {
   return (
@@ -6,6 +9,16 @@ function App() {
       <header className='app-header'>
         <h1>Book app 222</h1>
       </header>
+      <main>
+        <div className='left-column'>
+          <BookForm/>
+        </div>
+
+        <div className='right-column'>
+          <Filter/>
+          <BookList/>
+        </div>
+      </main>
     </div>
   );
 }
