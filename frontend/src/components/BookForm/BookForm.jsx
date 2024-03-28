ef726b155bdc2041f3ea7e27269e9ef97a4455b0
Kiril <mikasaxd@gmail.com>
2024-03-28 16:18:32 +0200

    add handler for bookList components

--- frontend/src/components/BookForm/BookForm.jsx
+++ frontend/src/components/BookForm/BookForm.jsx
@@ -1,8 +1,29 @@
-
+import { useState } from 'react'
+import './BookForm.css'
 
 function BookForm() {
+   const [formData, setFormData] = useState({bookTitle: '',author: '' });
+
+    const submitForm = (event) => {
+        event.preventDefault();
+
+        if(formData.bookTitle && formData.author) {
+            setFormData({bookTitle: '',author: ''})
+        }
+    }
+    
   return (
-    <div>BookForm</div>
+        <form className="bookForm" onSubmit={submitForm}>
+            <label>
+                Book title
+                <input type="text" value={formData.bookTitle} onChange={(e) => setFormData({...formData, bookTitle: e.target.value})}  placeholder="book title"/>
+            </label>
+            <label>
+                Book author
+                <input type="text" value={formData.author} onChange={(e) => setFormData({...formData, author: e.target.value })} placeholder="author"/>
+            </label>
+            <button type='submit'>Add Book</button>
+        </form>
   )
 }
 
